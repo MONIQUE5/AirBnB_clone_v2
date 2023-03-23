67d140a3878c87f21669a12fd2dfa83f05e38b03
Monica Aneke <anekemonica.am@gmail.com>
2023-03-23 03:04:45 +0100

    Group Project on AirBnB consisting of python and mysql

---
+++ README.md
@@ -0,0 +1,2 @@
+#!/usr/bin/python3
+This GRoup project was done by Monica Aneke and Jerry Nwadinobi
---
+++ git-script.sh
@@ -0,0 +1,3 @@
+git add -A
+git commit -m "Group Project on AirBnB consisting of python and mysql"
+git push
